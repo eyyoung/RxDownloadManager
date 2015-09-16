0f945ad6c62a7009bf440d78f10f4448b85f3813
eyyoung <young91@vip.qq.com>
2015-09-16 19:38:03 +0800

    增加测试用例

---
+++ library/src/androidTest/java/com/nd/android/sdp/dm/presenter/DownloadPresenterTest.java
@@ -0,0 +1,40 @@
+package com.nd.android.sdp.dm.presenter;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.nd.android.sdp.dm.options.DownloadOptions;
+import com.nd.android.sdp.dm.service.presenter.DownloadPresenter;
+
+import org.junit.runner.RunWith;
+
+/**
+ * 下载Presenter测试
+ * {@link DownloadPresenter}
+ *
+ * @author Young
+ */
+@RunWith(AndroidJUnit4.class)
+public class DownloadPresenterTest {
+
+    /**
+     * 测试添加任务
+     * {@link DownloadPresenter#addTask(String, String, DownloadOptions)}
+     *
+     * @author Young
+     */
+    public void testAddTask() {
+
+    }
+
+    /**
+     * 测试重复添加任务
+     * {@link DownloadPresenter#addTask(String, String, DownloadOptions)}
+     * A. 文件下载完成 B.正在下载中
+     *
+     * @author Young
+     */
+    public void testAddTaskRepeat() {
+
+    }
+
+}
--- library/src/test/java/com/nd/sdp/android/downloader/BaseDownloaderTest.java
+++ library/src/test/java/com/nd/sdp/android/downloader/BaseDownloaderTest.java
@@ -1,6 +1,7 @@
 package com.nd.sdp.android.downloader;
 
 import com.nd.android.sdp.dm.downloader.BaseDownloader;
+import com.nd.android.sdp.dm.exception.DownloadHttpException;
 import com.nd.android.sdp.dm.service.presenter.DownloadPresenter;
 
 import org.junit.Test;
@@ -16,13 +17,29 @@ import static junit.framework.Assert.assertTrue;
 public class BaseDownloaderTest {
 
     @Test
-    public void testGetStream() throws IOException {
+    public void testGetStream() throws IOException, DownloadHttpException {
         final BaseDownloader baseDownloader = new BaseDownloader();
         final InputStream stream = baseDownloader.getStream("http://apps.pba.cn/apk/Hardware_release.apk", null);
         byte[] bytes = new byte[DownloadPresenter.DEFAULT_BUFFER_SIZE];
         stream.read(bytes, 0, DownloadPresenter.DEFAULT_BUFFER_SIZE);
         stream.close();
         assertTrue(bytes[0] != 0);
+    }
+
+    /**
+     * 测试返回码是否正确
+     */
+    @Test
+    public void testDownloadFailed() {
+
+    }
+
+    /**
+     * 测试断点续传获取长度
+     */
+    @Test
+    public void testDownloadResume() {
+
     }
 
 }
